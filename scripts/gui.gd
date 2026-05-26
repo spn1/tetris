0fec123df2d5171002d99a44b0cf61f9d031fbf8
Spencer Newton <sn.newton92@gmail.com>
2026-05-26 20:55:49 +0100

    graphics for hold piece & piece queue

--- scripts/gui.gd
+++ scripts/gui.gd
@@ -4,8 +4,15 @@ extends CanvasLayer
 @onready var level: Label = $Container/RightPanel/GameInfo/Level
 @onready var lines: Label = $Container/RightPanel/GameInfo/Lines
 @onready var queue = $Container/RightPanel/Queue
+@onready var _queue_previews: Array = [
+	$Container/RightPanel/Queue/Control/TileMapLayer,
+	$Container/RightPanel/Queue/Control2/TileMapLayer,
+	$Container/RightPanel/Queue/Control3/TileMapLayer,
+	$Container/RightPanel/Queue/Control4/TileMapLayer,
+	$Container/RightPanel/Queue/Control5/TileMapLayer,
+]
 
-@onready var hold_piece: Label = $Container/LeftPanel/HoldPiece
+@onready var hold_piece = $Container/LeftPanel/HoldPiece/TileMapLayer
 
 
 ### ===============================
@@ -24,12 +31,20 @@ func on_lines_changed(_lines: int) -> void:
 
 
 func on_hold_piece_changed(_hold_piece: int) -> void:
-	hold_piece.text = str(_hold_piece)
+	var tile_map: TileMapLayer = hold_piece
+	tile_map.clear()
+
+	var atlas = Constants.PIECE_ATLAS_COORDS[_hold_piece]
+	for offset: Vector2i in Constants.PIECES[_hold_piece][Constants.Rotation.SPAWN]:
+		tile_map.set_cell(offset, 0, atlas)
 
 
 func on_queue_changed(_queue: Array):
-	var labels = queue.get_children()
-	for i in range(labels.size()):
+	for i in range(_queue_previews.size()):
+		var tile_map: TileMapLayer = _queue_previews[i]
+		tile_map.clear()
+
 		var piece_type = _queue[i]
-		var piece = Constants.Piece.keys()[piece_type]
-		labels[i].text = Constants.Piece.keys()[_queue[i]]
+		var atlas = Constants.PIECE_ATLAS_COORDS[piece_type]
+		for offset: Vector2i in Constants.PIECES[piece_type][Constants.Rotation.SPAWN]:
+			tile_map.set_cell(offset, 0, atlas)
